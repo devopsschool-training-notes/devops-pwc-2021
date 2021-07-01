9a802d25c19a335b6872d71e8377b09d827f71c3
DevOps School <44023974+devops-school@users.noreply.github.com>
2021-07-01 11:23:55 +0530

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+	aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "github" {}
